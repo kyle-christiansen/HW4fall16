24342f923c0b8973083a511cde0ba574b50907d7
Kyle Christiansen <kylec2013@gmail.com>
2016-09-28 21:54:25 +0000

    i hope this works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     def self.create_user!(user_params)
         user_params[:session_token] = SecureRandom.base64
-        User.create!(hash)
+        User.create!(user_params)
     end
 end
